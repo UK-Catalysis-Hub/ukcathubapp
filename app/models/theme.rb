fc0cf1265d0091e52c0dc93908c0fb7e748a2798
scman1 <a_nieva@hotmail.com>
2020-02-25 14:47:13 +0000

    display authors within themes

--- app/models/theme.rb
+++ app/models/theme.rb
@@ -1,4 +1,16 @@
 class Theme < ApplicationRecord
   has_many :article_themes
   has_many :articles, through: :article_themes
+  # custom method to get the list of authors for a given theme
+  def get_authors
+    authors = []
+    articles.each do |article|
+      article.authors.each do |author|
+        if not authors.include?(author) then
+          authors.push(author)
+        end
+      end
+    end
+    authors
+  end
 end
