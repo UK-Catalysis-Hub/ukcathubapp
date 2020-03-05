40b2d1ada913c952d6d6830282083331c73234c2
scman1 <a_nieva@hotmail.com>
2020-03-05 15:58:14 +0000

    added get address method to affiliation model

--- app/models/affiliation.rb
+++ app/models/affiliation.rb
@@ -4,4 +4,13 @@ class Affiliation < ApplicationRecord
   has_many :authors, through: :affiliation_links
   has_many :articles, through: :affiliation_links
   has_many :addresses, through: :affiliation_links
+  def get_addresses
+    addr_list = []
+    addresses.each do |affi_address|
+      if not addr_list.include?(affi_address)
+        addr_list.push(affi_address)
+      end
+    end
+    addr_list.sort()
+  end
 end
