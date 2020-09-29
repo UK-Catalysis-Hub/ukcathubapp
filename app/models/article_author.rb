915940c0e9c509434e165930723052c2007c96da
scman1 <a_nieva@hotmail.com>
2020-09-29 12:13:21 +0100

    edit author and link to article-author

--- app/models/article_author.rb
+++ app/models/article_author.rb
@@ -3,4 +3,7 @@ class ArticleAuthor < ApplicationRecord
   belongs_to :article
   has_many :cr_affiliations
   has_many :author_affiliations
+  # scopes
+  scope :not_verified, -> {where("status is not 'verified'") }
+  scope :verified, -> {where("status is 'verified'") }
 end
