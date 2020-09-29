3bda87edcfd835e62db45e254bd0fcce5f37f788
scman1 <a_nieva@hotmail.com>
2020-09-29 10:18:55 +0100

    checking author scopes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @latest_articles = Article.latest
     @cited_articles = Article.most_cited
+    @authors = Author.top_published
   end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -8,7 +8,8 @@ class Author < ApplicationRecord
   has_many :affiliations, through: :affiliation_links
 
   # scopes
-  scope :not_verified , -> {joins("INNER JOIN article_authors ON article_authors.author_id = authors.id").where("article_authors.status is not 'verified'") }
-  scope :top_published , -> {order('articles DESC').limit(10) }
-
+  scope :not_verified, -> {joins("INNER JOIN article_authors ON article_authors.author_id = authors.id").where("article_authors.status is not 'verified'") }
+  scope :top_published, -> {order('articles DESC').limit(10) }
+  scope :top_published2, -> {Article.active.joins(:authors).select("authors.id, full_name, COUNT(*) AS 'article_count'").order("article_count DESC").group("authors.full_name").limit(5)}
+  scope :active, -> {joins(:articles).where("articles.status = 'Added'")}
 end
