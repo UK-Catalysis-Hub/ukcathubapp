102f42777b80f548176d71e883aa0d93d91ee8f0
scman1 <a_nieva@hotmail.com>
2020-09-30 11:33:33 +0100

    show CR author affiliations in author show form

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,9 +1,13 @@
 class Author < ApplicationRecord
-  #relationships to articles
+  # relationships to articles
   has_many :article_authors
   has_many :articles, through: :article_authors
 
-  #relationships to affiliations
+  #extra relationships to intermediate Crossref objects
+  has_many :cr_affiliations, through: :article_authors
+  has_many :author_affiliations, through: :article_authors
+
+  # relationships to affiliations
   has_many :affiliation_links
   has_many :affiliations, through: :affiliation_links
 
