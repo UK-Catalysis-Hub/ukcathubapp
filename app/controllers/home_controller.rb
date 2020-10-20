4a98614a01478827b71c3585da1e825e7a55c78b
scman1 <a_nieva@hotmail.com>
2020-10-20 18:45:12 +0100

    removed obsolete scopes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,5 @@ class HomeController < ApplicationController
   def index
     @latest_articles = Article.latest
     @cited_articles = Article.most_cited
-    @authors = Author.top_published
   end
 end
