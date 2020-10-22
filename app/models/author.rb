f16c11f15a70f7dfd0549596796699c5c0d5f2a2
scman1 <a_nieva@hotmail.com>
2020-10-22 15:06:52 +0100

    added map for authors origins

--- app/models/author.rb
+++ app/models/author.rb
@@ -16,4 +16,6 @@ class Author < ApplicationRecord
   scope :active, -> {joins(:articles).where("articles.status = 'Added'").group("authors.id")}
   scope :publications_count, -> {active.joins(:articles).select("authors.id, full_name, COUNT(*) AS 'pub_count'").order("pub_count DESC").group("authors.full_name")}
   scope :citations_count, -> {active.joins(:articles).select("authors.id, full_name, SUM(articles.referenced_by_count) AS 'Citations'").order("Citations DESC").group("authors.full_name")}
+  scope :country_count, -> {Author.joins(:author_affiliations).select('author_affiliations.country').group(:country).count('*')}
+
 end
