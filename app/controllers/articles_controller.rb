0dbff86c54bcd2858ca0fc57012b5c85fcea12c8
scman1 <a_nieva@hotmail.com>
2020-12-02 14:39:00 +0000

    only attempt retrieve from crossref if doi is valid

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -106,9 +106,6 @@ class ArticlesController < ApplicationController
            #save the article so it is not recovered again
            @article.save()
          else
-           puts "############################################################"
-           puts "Article already exists in db"
-           puts "############################################################"
            @article= @art
          end
       end
@@ -323,67 +320,69 @@ class ArticlesController < ApplicationController
       # get the article from crossref
       doi_text = article.doi
       art_id = article.id
-      pub_data = getCRData(doi_text)
-      changes_found = false
-      # only verify fields which may chenge between recoveries, eg: citations
-      if article.attributes['referenced_by_count'] != pub_data['is-referenced-by-count']
-        changes_found = true
-        article.attributes['referenced_by_count'] = pub_data['is-referenced-by-count']
-        article.referenced_by_count = pub_data['is-referenced-by-count']
-      end
-      if changes_found
-        article.save
-      end
-      aut_order = 1
-      pub_data['author'].each do |art_author|
+      if article.doi != ""
+        pub_data = getCRData(doi_text)
         changes_found = false
-        new_author = ArticleAuthor.new()
-        if art_id != nil
-          tem_auth = ArticleAuthor.find_by article_id: art_id, author_order: aut_order
-          if tem_auth != nil
-            new_author = tem_auth
-          end
-        end
-        if art_author.keys.include?('ORCID')
-          if  new_author.orcid != art_author['ORCID']
-            changes_found = true
-            new_author.orcid = art_author['ORCID']
-          end
-        end
-        if art_author.keys.include?('family')
-          if new_author.last_name != art_author['family']
-            changes_found = true
-            new_author.last_name = art_author['family']
-          end
-        end
-        if art_author.keys.include?('given')
-          if new_author.given_name != art_author['given']
-            changes_found = true
-            new_author.given_name = art_author['given']
-          end
+        # only verify fields which may chenge between recoveries, eg: citations
+        if article.attributes['referenced_by_count'] != pub_data['is-referenced-by-count']
+          changes_found = true
+          article.attributes['referenced_by_count'] = pub_data['is-referenced-by-count']
+          article.referenced_by_count = pub_data['is-referenced-by-count']
         end
         if changes_found
-          new_author.save
+          article.save
         end
-        if art_author.keys.include?('affiliation')
-          if art_author['affiliation'].length > 0
-            art_author['affiliation'].each do |temp_affi|
-              new_affi = CrAffiliation.new()
-              affi_text = temp_affi['name']
-              if aut_id != nil
-                tem_affi = CrAffiliation.find_by article_author_id: aut_id, name: affi_text
-                if tem_affi != nil
-                  new_affi = tem_affi
-                else
-                  new_affi.name = temp_affi['name']
-                  new_affi.article_author_id = new_author.id
-                  new_affi.save
+        aut_order = 1
+        pub_data['author'].each do |art_author|
+          changes_found = false
+          new_author = ArticleAuthor.new()
+          if art_id != nil
+            tem_auth = ArticleAuthor.find_by article_id: art_id, author_order: aut_order
+            if tem_auth != nil
+              new_author = tem_auth
+            end
+          end
+          if art_author.keys.include?('ORCID')
+            if  new_author.orcid != art_author['ORCID']
+              changes_found = true
+              new_author.orcid = art_author['ORCID']
+            end
+          end
+          if art_author.keys.include?('family')
+            if new_author.last_name != art_author['family']
+              changes_found = true
+              new_author.last_name = art_author['family']
+            end
+          end
+          if art_author.keys.include?('given')
+            if new_author.given_name != art_author['given']
+              changes_found = true
+              new_author.given_name = art_author['given']
+            end
+          end
+          if changes_found
+            new_author.save
+          end
+          if art_author.keys.include?('affiliation')
+            if art_author['affiliation'].length > 0
+              art_author['affiliation'].each do |temp_affi|
+                new_affi = CrAffiliation.new()
+                affi_text = temp_affi['name']
+                if aut_id != nil
+                  tem_affi = CrAffiliation.find_by article_author_id: aut_id, name: affi_text
+                  if tem_affi != nil
+                    new_affi = tem_affi
+                  else
+                    new_affi.name = temp_affi['name']
+                    new_affi.article_author_id = new_author.id
+                    new_affi.save
+                  end
                 end
               end
             end
           end
+          aut_order += 1
         end
-        aut_order += 1
       end
     end
 
