e69a6ed0314f0f92d0d8b06e1a7edf7a0cd84eea
scman1 <a_nieva@hotmail.com>
2020-12-16 14:29:02 +0000

    added statistics per year to data objects

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -6,4 +6,5 @@ class Dataset < ApplicationRecord
   #scopes for datsets
   scope :latest, -> {order('dataset_startdate DESC').order('id DESC').limit(10)  }
   scope :repository_count, -> {select("repository, COUNT(*) AS 'r_count'").order("r_count DESC").group("repository")}
+  scope :ds_per_year, -> {select("strftime('%Y', dataset_startdate) AS item, COUNT(*) AS 'i_count'").group("item")}
 end
