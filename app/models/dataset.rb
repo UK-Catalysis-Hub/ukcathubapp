61720b455c3db36cce74772179c3f7bcf5aae2f1
scman1 <a_nieva@hotmail.com>
2020-12-16 11:54:43 +0000

    added statistics per publisher

--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -3,6 +3,7 @@ class Dataset < ApplicationRecord
   has_many :article_datasets
   has_many :articles, through: :article_datasets
 
+  #scopes for datsets
   scope :latest, -> {order('dataset_startdate DESC').order('id DESC').limit(10)  }
-
+  scope :repository_count, -> {select("repository, COUNT(*) AS 'r_count'").order("r_count DESC").group("repository")}
 end
