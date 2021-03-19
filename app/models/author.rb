4b547bca76b4f55e8887f24e456e889bcca772df
Abraham Nieva <a_nieva@hotmail.com>
2021-03-19 18:35:39 +0000

    added message explainign limit to personal data publishing

--- app/models/author.rb
+++ app/models/author.rb
@@ -15,8 +15,8 @@ class Author < ApplicationRecord
   scope :isap, -> {where("authors.isap = 1")}
   scope :not_verified, -> {joins("INNER JOIN article_authors ON article_authors.author_id = authors.id").where("article_authors.status is not 'verified'") }
   scope :active, -> {joins(:articles).where("articles.status = 'Added'").group("authors.id")}
-  scope :publications_count, -> {active.joins(:articles).select("authors.id, full_name, COUNT(*) AS 'pub_count'").order("pub_count DESC").group("authors.full_name")}
-  scope :citations_count, -> {active.joins(:articles).select("authors.id, full_name, SUM(articles.referenced_by_count) AS 'Citations'").order("Citations DESC").group("authors.full_name")}
+  scope :publications_count, -> {isap.joins(:articles).select("authors.id, full_name, COUNT(*) AS 'pub_count'").order("pub_count DESC").group("authors.full_name")}
+  scope :citations_count, -> {isap.joins(:articles).select("authors.id, full_name, SUM(articles.referenced_by_count) AS 'Citations'").order("Citations DESC").group("authors.full_name")}
   scope :country_count, -> {Author.joins(:author_affiliations).select('author_affiliations.country').group(:country).count('*')}
 
 end
