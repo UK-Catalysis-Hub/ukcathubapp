21a4b946aec21008854edc15f03bb7881b3dcd50
Abraham Nieva <a_nieva@hotmail.com>
2021-05-18 19:20:05 +0100

    checking affiliation data

--- app/models/author.rb
+++ app/models/author.rb
@@ -19,5 +19,4 @@ class Author < ApplicationRecord
   scope :data_objects_count, -> {isap.joins(:articles).joins("INNER JOIN article_datasets ON article_datasets.article_id = articles.id").where("article_datasets.article_id = articles.id").select("authors.id, full_name, COUNT(*) AS 'pub_count'").order("pub_count DESC").group("authors.full_name")}
   scope :citations_count, -> {isap.joins(:articles).select("authors.id, full_name, SUM(articles.referenced_by_count) AS 'Citations'").order("Citations DESC").group("authors.full_name")}
   scope :country_count, -> {Author.joins(:author_affiliations).select('author_affiliations.country').group(:country).count('*')}
-
 end
