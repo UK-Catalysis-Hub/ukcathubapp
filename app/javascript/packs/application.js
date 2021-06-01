6218b71ce0f2ecf3e9828d337ae85f9fe88fb0ba
Abraham Nieva <a_nieva@hotmail.com>
2021-06-01 19:15:57 +0100

    corrected path to scss 02

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require("chartkick")
 require("chart.js")
 // require('bootstrap-sprockets')
 import "bootstrap"
-import "../../stylesheets/application"
+import "../stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
