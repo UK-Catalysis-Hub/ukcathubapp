b97624ae0ce998341c543bd9077a7c5adb586c12
Abraham Nieva <a_nieva@hotmail.com>
2022-05-24 23:42:19 +0100

    removed duplicated stylesheet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ require("chartkick")
 require("chart.js")
 // require('bootstrap-sprockets')
 import "bootstrap"
-import "../stylesheets/application"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
