d870c7f2d118bb554c9dad70509cbdd627fec8cc
Abraham Nieva <a_nieva@hotmail.com>
2022-09-26 17:48:14 +0100

    changed to rails 6.1.3+ and ruby 3.0

--- app/models/article.rb
+++ app/models/article.rb
@@ -14,7 +14,7 @@ class Article < ApplicationRecord
 
   scope :active, -> {where(status: "Added")  }
   scope :inactive, -> {where("status <> ?", "Added")  }
-  scope :latest, -> {where(status: "Added").order('CASE WHEN Articles.pub_ol_year = "" THEN Articles.pub_print_year ELSE Articles.pub_ol_year END DESC').order('id DESC').limit(10)  }
+  scope :latest, -> {where(status: "Added").order("pub_year DESC").order('id DESC').limit(10)  }
   scope :most_cited, -> {where(status: "Added").order('referenced_by_count DESC').order('id DESC').limit(10) }
   scope :journals_count, -> {active.select("container_title, COUNT(*) AS 'j_count'").order("j_count DESC").group("container_title")}
   scope :publisher_count, -> {active.select("publisher, COUNT(*) AS 'p_count'").order("p_count DESC").group("publisher")}
