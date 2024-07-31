ce0f474ca86b6308145e2036a307a48333d30fac
Abraham <a_nieva@hotmail.com>
2024-07-31 16:34:43 +0100

    set eager_load to true

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,20 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.19.1"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "ukchapp"
+set :repo_url, "https://github.com/UK-Catalysis-Hub/ukcathubapp.git"
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
 # Default deploy_to directory is /var/www/my_app_name
 # set :deploy_to, "/var/www/my_app_name"
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', "storage"
+
+#keep last 5 releases on redeployment
+set :keep_releases, 5
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
