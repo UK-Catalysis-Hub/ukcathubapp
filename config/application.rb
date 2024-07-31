121b00de85471fc2eb42d3619a667af1fa59f0d8
Abraham <a_nieva@hotmail.com>
2024-07-31 16:44:53 +0100

    undo eager_load to true

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module Ukchsimple
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.eager_load = True
+    
   end
 end
