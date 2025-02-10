f4c496bcbf6f8a57c57cb4084735106c702bfc91
Abraham <a_nieva@hotmail.com>
2025-02-10 23:49:59 +0000

    habilitated the add theme button

--- app/controllers/article_themes_controller.rb
+++ app/controllers/article_themes_controller.rb
@@ -57,6 +57,35 @@ class ArticleThemesController < ApplicationController
     end
   end
 
+  # Assign theme to article record
+  def link_to_theme
+    # get the theme id from the parameters
+    # get the article id from the parameters
+    # get the project_year from the parameters
+    # save article_theme
+    # redirect to article edit
+    puts "********************************************************************"
+    puts params
+    puts params["data"]
+    this_theme = Theme.find(params["data"]["id"])
+    this_article = Article.find(params["data"]["article_id"])
+    @article_theme = ArticleTheme.new()
+    @article_theme.doi = this_article.doi
+    @article_theme.project_year = params["data"]["year"]
+    @article_theme.theme_id = params["data"]["id"]
+    @article_theme.phase = this_theme.phase
+    @article_theme.collaboration = 0
+    @article_theme.article_id = params["data"]["article_id"]
+    @article_theme.save()
+    puts "********************************************************************"
+     respond_to do |format|
+       flash[:notice] = 'Article linked to theme'
+       return_to = session[:return_to]
+       return_to.include?("/edit") ? return_to : return_to = return_to + "/edit"
+       format.html { redirect_to return_to }
+     end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_article_theme
