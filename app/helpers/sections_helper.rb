ac602af9df598812aee1c47dad3dbdc5ac80dc97
Abraham <a_nieva@hotmail.com>
2025-03-12 23:29:15 +0000

    moved code for sections data to sections helper

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,2 +1,27 @@
 module SectionsHelper
+
+  def get_article_data
+    # Fetch your model data here
+    Section.where("obj_name='Articles'")[0]
+  end
+
+  def get_author_data
+    # Fetch your model data here
+    Section.where("obj_name='Authors'")[0]
+  end
+
+  def get_affiliation_data
+    # Fetch your model data here
+    Section.where("obj_name='Affiliations'")[0]
+  end
+
+  def get_theme_data
+    # Fetch your model data here
+    Section.where("obj_name='Themes'")[0]
+  end
+
+  def get_do_data
+    # Fetch your model data here
+    Section.where("obj_name='Datasets'")[0]
+  end
 end
