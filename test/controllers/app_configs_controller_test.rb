f23a6d5ed71967a02d681f6f747a8838091e1cc2
Abraham <a_nieva@hotmail.com>
2025-03-18 01:37:06 +0000

    fix tests. Added confirmed at manually to fix redirects

--- test/controllers/app_configs_controller_test.rb
+++ test/controllers/app_configs_controller_test.rb
@@ -2,30 +2,8 @@ require "test_helper"
 
 class AppConfigsControllerTest < ActionDispatch::IntegrationTest
   setup do
+    sign_in users(:one)
     @app_config = app_configs(:one)
-  end
-
-  test "should get index" do
-    get app_configs_url
-    assert_response :success
-  end
-
-  test "should get new" do
-    get new_app_config_url
-    assert_response :success
-  end
-
-  test "should create app_config" do
-    assert_difference("AppConfig.count") do
-      post app_configs_url, params: { app_config: { award_list: @app_config.award_list, browser_tab_name: @app_config.browser_tab_name, contact_email: @app_config.contact_email, contact_id: @app_config.contact_id, organisation_id: @app_config.organisation_id, synon_list: @app_config.synon_list, title: @app_config.title } }
-    end
-
-    assert_redirected_to app_config_url(AppConfig.last)
-  end
-
-  test "should show app_config" do
-    get app_config_url(@app_config)
-    assert_response :success
   end
 
   test "should get edit" do
@@ -38,11 +16,4 @@ class AppConfigsControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to app_config_url(@app_config)
   end
 
-  test "should destroy app_config" do
-    assert_difference("AppConfig.count", -1) do
-      delete app_config_url(@app_config)
-    end
-
-    assert_redirected_to app_configs_url
-  end
 end
