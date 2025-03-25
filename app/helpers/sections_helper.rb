61507161d4b02825fb98f4cad79ba2f9ab7496f5
Abraham <a_nieva@hotmail.com>
2025-03-25 12:09:56 +0000

    added route for service. Works OK

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,32 +1,26 @@
 module SectionsHelper
-
+  # Methods to fetch section data
   def get_article_section
-    # Fetch your model data here
     Section.where("obj_name='Articles'")[0]
   end
 
   def get_author_section
-    # Fetch your model data here
     Section.where("obj_name='Authors'")[0]
   end
 
   def get_affiliation_section
-    # Fetch your model data here
     Section.where("obj_name='Affiliations'")[0]
   end
 
   def get_theme_section
-    # Fetch your model data here
     Section.where("obj_name='Themes'")[0]
   end
 
   def get_do_section
-    # Fetch your model data here
     Section.where("obj_name='Datasets'")[0]
   end
   
   def get_home_section
-    # Fetch your model data here
     Section.where("obj_name='Home'")[0]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,29 +15,33 @@ Rails.application.routes.draw do
   resources :article_authors
   resources :affiliations
   resources :addresses
-  
+
   get 'home/index'
   root 'home#index'
-   
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # download article stats csv
   get 'get_art_stats' => 'articles#arts_by_year_stats', as: 'get_art_stats'
-  
+
   get 'ctry_stats' => 'affiliations#ctry_affi_count', as: 'ctry_stats'
   get 'themes_count' => 'themes#themes_count', as: 'themes_count'
-  
+
   post 'link_to_theme' => 'article_themes#link_to_theme', as: :link_to_theme
-  
+
   post 'verify' => 'articles#verify', as: :verify
   post 'upload_csv' => 'articles#upload_csv', as: :upload_csv
-  
+
   get 'data_count' => 'datasets#data_count', as: 'data_count'
   post 'upload_data' => 'datasets#upload_data', as: 'upload_data'
-  
+
   post 'link_to_researcher' => 'article_authors#link_to_researcher', as: :link_to_researcher
-  
+
   post 'generate_affiliations' => 'authors#generate', as: :generate_auth_affis
+
+  # get publications bibliography service
+  get 'get_pubs' => 'articles#bib_query', as: :getpubs
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
