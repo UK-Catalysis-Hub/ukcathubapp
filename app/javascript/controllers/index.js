b22d66f3490cd3acb766b55d482fbccb4e904804
Abraham <a_nieva@hotmail.com>
2025-04-16 16:12:13 +0100

    stub for js for theme mapper code

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import ThemeMapperController from "./theme_mapper_controller"
+application.register("theme-mapper", ThemeMapperController)
