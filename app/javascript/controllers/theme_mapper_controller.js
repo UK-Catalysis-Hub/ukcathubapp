7cfb285afa301d65d7d3485344f2d87e81a21bc5
Abraham <a_nieva@hotmail.com>
2025-04-23 17:33:06 +0100

    assign themes control tested manually

--- app/javascript/controllers/theme_mapper_controller.js
+++ app/javascript/controllers/theme_mapper_controller.js
@@ -3,10 +3,10 @@ import { Controller } from "@hotwired/stimulus"
 // Connects to data-controller="theme-mapper"
 export default class extends Controller {
   connect(){
-    console.log("IssueMapperController is connected!");
+    //console.log("IssueMapperController is connected!");
   }
+
   moveTheme(event) {
-    console.log("move theme called!");
     const fromList = this.element.querySelector(`#${event.target.dataset.from}`);
     const toList = this.element.querySelector(`#${event.target.dataset.to}`);
 
@@ -18,13 +18,9 @@ export default class extends Controller {
   }
 
   validateSelection() {
-    console.log("Validating Selection")
     const assignedList = this.element.querySelector('#assigned_themes');
     const themesList = document.getElementById('theme_ids');
     var selectedIds = Array.from(assignedList.options).map(option => option.value);
-    console.log(selectedIds.join(","));
     themesList.value = selectedIds.join(","); 
-    // const submitButton = this.element.querySelector('#submit_button');
-    // submitButton.disabled = assignedList.options.length === 0;
   }
 }
