2752ec2f0caa5be4c151824329cec0eb61248254
Abraham <a_nieva@hotmail.com>
2025-05-23 13:47:14 +0100

    fixed logger level error

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -3,8 +3,12 @@ require 'sidekiq-cron'
 
 Sidekiq.configure_server do |config|
   config.logger = Rails.logger
-  config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
-  config.logger.level = Logger:DEBUG
+  #config.logger.formatter = Sidekiq::Logger::Formatters::JSON.new
+  config.logger.formatter = proc do |severity,datetime, progname, message|
+    "[#{datetime.strftime('%Y-%m-%d %H:%M:%S')}] #{progname} #{severity}: #{message}"
+  end
+
+  config.logger.level = Logger::DEBUG
   config.on(:startup) do
     schedule_file = Rails.root.join('config', 'sidekiq_schedule.yml')
 
