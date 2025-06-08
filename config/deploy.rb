fa41f80ae7d3e09acc08cc646beabb493ad97668
Abraham <a_nieva@hotmail.com>
2025-06-08 16:04:07 +0100

    mod capistrano start sidekiq after deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -50,23 +50,8 @@ namespace :sidekiq do
   task :setup do
     on roles(:app) do
       execute <<~EOS
-        sudo bash -c 'cat > /etc/systemd/system/sidekiq.service <<EOF
-        [Unit]
-        Description=Sidekiq background worker
-        After=syslog.target network.target
-
-        [Service]
-        Type=simple
-        User=deploy
-        Group=deploy
-        WorkingDirectory=#{fetch(:deploy_to)}/current
-        ExecStart=/home/deploy/.rbenv/shims/bundle exec sidekiq -e production -C #{fetch(:deploy_to)}/current/config/sidekiq.yml
-        RestartSec=5
-        Restart=always
-
-        [Install]
-        WantedBy=multi-user.target
-        EOF'
+#        echo -e "[Unit]\\nDescription=Sidekiq Background Worker\\nAfter=syslog.target network.target\\n\\n[Service]\\nType=simple\\nUser=deploy\\nGroup=deploy\\nWorkingDirectory=#{fetch(:deploy_to)}/current\\nExecStart=/home/deploy/ukchapp/current/bin/bundle exec sidekiq -e production -C #{fetch(:deploy_to)}/current/config/sidekiq.yml\\nRestartSec=5\\nRestart=always\\n\\n[Install]\\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/sidekiq.service
+        echo -e "[Unit]\\nDescription=Sidekiq Background Worker\\nAfter=network.target\\n[Service]\\nEnvironment="RAILS_ENV=production"\\nExecStart=/home/deploy/.rbenv/shims/bundle exec sidekiq\\nRestart=always\\nUser=deploy\\nWorkingDirectory=/home/deploy/ukchapp/current\\n[Install]\\nWantedBy=multi-user.target"|sudo tee /etc/systemd/system/sidekiq.service
       EOS
       execute 'sudo systemctl daemon-reload'
       execute 'sudo systemctl enable sidekiq'
