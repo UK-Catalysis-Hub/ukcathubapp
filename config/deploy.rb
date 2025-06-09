c7d85f5cb764421b12b6a1fb9a9020b883b67a29
Abraham <a_nieva@hotmail.com>
2025-06-09 01:04:53 +0100

    add sidekiq service to systemctl so that it is started automatically on deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -45,12 +45,13 @@ after 'deploy:publishing', 'sidekiq:restart'
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
 
+#        echo -e "[Unit]\\nDescription=Sidekiq Background Worker\\nAfter=syslog.target network.target\\n\\n[Service]\\nType=simple\\nUser=deploy\\nGroup=deploy\\nWorkingDirectory=#{fetch(:deploy_to)}/current\\nExecStart=/home/deploy/ukchapp/current/bin/bundle exec sidekiq -e production -C #{fetch(:deploy_to)}/current/config/sidekiq.yml\\nRestartSec=5\\nRestart=always\\n\\n[Install]\\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/sidekiq.service
+
 namespace :sidekiq do
   desc 'Setup Sidekiq systemd service'
   task :setup do
     on roles(:app) do
       execute <<~EOS
-#        echo -e "[Unit]\\nDescription=Sidekiq Background Worker\\nAfter=syslog.target network.target\\n\\n[Service]\\nType=simple\\nUser=deploy\\nGroup=deploy\\nWorkingDirectory=#{fetch(:deploy_to)}/current\\nExecStart=/home/deploy/ukchapp/current/bin/bundle exec sidekiq -e production -C #{fetch(:deploy_to)}/current/config/sidekiq.yml\\nRestartSec=5\\nRestart=always\\n\\n[Install]\\nWantedBy=multi-user.target" | sudo tee /etc/systemd/system/sidekiq.service
         echo -e "[Unit]\\nDescription=Sidekiq Background Worker\\nAfter=syslog.target network.target\\n[Service]\\nEnvironment="RAILS_ENV=production"\\nExecStart=/home/deploy/.rbenv/shims/bundle exec sidekiq\\nRestart=always\\nUser=deploy\\nWorkingDirectory=/home/deploy/ukchapp/current\\n[Install]\\nWantedBy=multi-user.target"|sudo tee /etc/systemd/system/sidekiq.service
       EOS
       execute 'sudo systemctl daemon-reload'
