38733747c5ec67af05074d5fd4638a833354feb4
Abraham <a_nieva@hotmail.com>
2025-07-21 18:13:40 +0100

    added dash to theme names to identify tabs on index

--- app/helpers/themes_helper.rb
+++ app/helpers/themes_helper.rb
@@ -1,6 +1,6 @@
 module ThemesHelper
   def get_theme_groupings()
-    x = Theme.group(:theme_type, :phase).order(:phase).count.to_a
+    x = Theme.group(:theme_type, :phase).order(phase: :desc).count.to_a
     return x.map {|(thm,thmty),thmc |[thm,thmty,thmc]}
   end
 end
