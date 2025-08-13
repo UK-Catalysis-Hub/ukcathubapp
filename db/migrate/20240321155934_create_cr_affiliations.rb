f627abc47dedd81e5e909b370f07fff61fefdac0
Abraham <a_nieva@hotmail.com>
2025-08-13 14:22:09 +0100

    changed field types for cr_affiliation ids

--- db/migrate/20240321155934_create_cr_affiliations.rb
+++ db/migrate/20240321155934_create_cr_affiliations.rb
@@ -2,8 +2,8 @@ class CreateCrAffiliations < ActiveRecord::Migration[7.1]
   def change
     create_table :cr_affiliations do |t|
       t.string :name
-      t.string :article_author_id
-      t.string :author_affiliation_id
+      t.integer :article_author_id
+      t.integer :author_affiliation_id
 
       t.timestamps
     end
