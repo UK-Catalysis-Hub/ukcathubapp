70039edccf11c39abedccd5b7ee017d74b6e9a82
Abraham <a_nieva@hotmail.com>
2025-09-04 00:26:46 +0100

    fixed affiliations and fistures for article_themes controller tests for postgre fixtures

--- test/controllers/article_themes_controller_test.rb
+++ test/controllers/article_themes_controller_test.rb
@@ -2,11 +2,11 @@ require "test_helper"
 
 class ArticleThemesControllerTest < ActionDispatch::IntegrationTest
   setup do
-    sign_in users(:one)
-    @article_theme = article_themes(:one)
+    sign_in users(:userone)
+    @article_theme = article_themes(:atone)
     # ids should not be empty "for belongs" to relationships
-    @article_theme.theme_id = themes(:one).id
-    @article_theme.article_id = articles(:one).id
+    @article_theme.theme_id = themes(:themeone).id
+    @article_theme.article_id = articles(:artone).id
   end
 
   test "should get index" do
