878cbb5e7748f2d10aeb319a64c36ce25b84987e
Abraham <a_nieva@hotmail.com>
2025-09-05 12:27:05 +0100

    added migration script for postgresql using pgloader

--- db/schema.rb
+++ db/schema.rb
@@ -15,278 +15,278 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_03_145548) do
   enable_extension "plpgsql"
 
   create_table "active_storage_attachments", force: :cascade do |t|
-    t.string "name", null: false
-    t.string "record_type", null: false
-    t.bigint "record_id", null: false
-    t.bigint "blob_id", null: false
-    t.datetime "created_at", null: false
-    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
-    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+    t.text "name"
+    t.text "record_type"
+    t.bigint "record_id"
+    t.bigint "blob_id"
+    t.timestamptz "created_at"
+    t.index ["blob_id"], name: "idx_24701_index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "idx_24701_index_active_storage_attachments_uniqueness", unique: true
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
-    t.string "key", null: false
-    t.string "filename", null: false
-    t.string "content_type"
+    t.text "key"
+    t.text "filename"
+    t.text "content_type"
     t.text "metadata"
-    t.string "service_name", null: false
-    t.bigint "byte_size", null: false
-    t.string "checksum"
-    t.datetime "created_at", null: false
-    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+    t.text "service_name"
+    t.bigint "byte_size"
+    t.text "checksum"
+    t.timestamptz "created_at"
+    t.index ["key"], name: "idx_24694_index_active_storage_blobs_on_key", unique: true
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.bigint "blob_id", null: false
-    t.string "variation_digest", null: false
-    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+    t.bigint "blob_id"
+    t.text "variation_digest"
+    t.index ["blob_id", "variation_digest"], name: "idx_24708_index_active_storage_variant_records_uniqueness", unique: true
   end
 
   create_table "addresses", force: :cascade do |t|
-    t.string "add_01"
-    t.string "add_02"
-    t.string "add_03"
-    t.string "add_04"
-    t.string "city"
-    t.string "province"
-    t.string "country"
-    t.integer "affiliation_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "add_01"
+    t.text "add_02"
+    t.text "add_03"
+    t.text "add_04"
+    t.text "city"
+    t.text "province"
+    t.text "country"
+    t.bigint "affiliation_id"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "affiliations", force: :cascade do |t|
-    t.string "institution"
-    t.string "department"
-    t.string "faculty"
-    t.string "school"
-    t.string "work_group"
-    t.string "country"
-    t.string "sector"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.integer "organisation_id"
+    t.text "institution"
+    t.text "department"
+    t.text "faculty"
+    t.text "school"
+    t.text "work_group"
+    t.text "country"
+    t.text "sector"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
+    t.bigint "organisation_id"
   end
 
   create_table "app_configs", force: :cascade do |t|
-    t.string "title"
-    t.string "browser_tab_name"
-    t.integer "organisation_id"
-    t.integer "contact_id"
-    t.string "contact_email"
-    t.string "award_list"
-    t.string "synon_list"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "title"
+    t.text "browser_tab_name"
+    t.bigint "organisation_id"
+    t.bigint "contact_id"
+    t.text "contact_email"
+    t.text "award_list"
+    t.text "synon_list"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "article_authors", force: :cascade do |t|
-    t.string "doi"
-    t.integer "author_id"
-    t.string "author_count"
-    t.integer "author_order"
-    t.string "status"
-    t.string "author_seq"
-    t.integer "article_id"
-    t.string "orcid"
-    t.string "last_name"
-    t.string "given_name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "doi"
+    t.bigint "author_id"
+    t.text "author_count"
+    t.bigint "author_order"
+    t.text "status"
+    t.text "author_seq"
+    t.bigint "article_id"
+    t.text "orcid"
+    t.text "last_name"
+    t.text "given_name"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "article_datasets", force: :cascade do |t|
-    t.string "doi"
-    t.integer "article_id"
-    t.integer "dataset_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "doi"
+    t.bigint "article_id"
+    t.bigint "dataset_id"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "article_themes", force: :cascade do |t|
-    t.string "doi"
-    t.integer "phase"
-    t.string "collaboration"
-    t.integer "theme_id"
-    t.integer "article_id"
-    t.integer "project_year"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "doi"
+    t.bigint "phase"
+    t.text "collaboration"
+    t.bigint "theme_id"
+    t.bigint "article_id"
+    t.bigint "project_year"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "articles", force: :cascade do |t|
-    t.string "doi"
-    t.string "title"
-    t.integer "pub_year"
-    t.string "pub_type"
-    t.string "publisher"
-    t.string "container_title"
-    t.string "volume"
-    t.string "issue"
-    t.string "page"
-    t.integer "pub_print_year"
-    t.integer "pub_print_month"
-    t.integer "pub_print_day"
-    t.integer "pub_ol_year"
-    t.integer "pub_ol_month"
-    t.integer "pub_ol_day"
-    t.string "license"
-    t.integer "referenced_by_count"
-    t.string "link"
-    t.string "url"
+    t.text "doi"
+    t.text "title"
+    t.bigint "pub_year"
+    t.text "pub_type"
+    t.text "publisher"
+    t.text "container_title"
+    t.text "volume"
+    t.text "issue"
+    t.text "page"
+    t.bigint "pub_print_year"
+    t.bigint "pub_print_month"
+    t.bigint "pub_print_day"
+    t.bigint "pub_ol_year"
+    t.bigint "pub_ol_month"
+    t.bigint "pub_ol_day"
+    t.text "license"
+    t.bigint "referenced_by_count"
+    t.text "link"
+    t.text "url"
     t.text "abstract"
-    t.string "status"
-    t.string "comment"
-    t.integer "references_count"
-    t.string "journal_issue"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "pdf_file"
-    t.string "graphic_abstract"
+    t.text "status"
+    t.text "comment"
+    t.bigint "references_count"
+    t.text "journal_issue"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
+    t.text "pdf_file"
+    t.text "graphic_abstract"
   end
 
   create_table "author_affiliations", force: :cascade do |t|
-    t.integer "article_author_id"
-    t.string "name"
-    t.string "short_name"
-    t.string "add_01"
-    t.string "add_02"
-    t.string "add_03"
-    t.string "add_04"
-    t.string "add_05"
-    t.string "city"
-    t.string "province"
-    t.string "country"
-    t.integer "affiliation_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "article_author_id"
+    t.text "name"
+    t.text "short_name"
+    t.text "add_01"
+    t.text "add_02"
+    t.text "add_03"
+    t.text "add_04"
+    t.text "add_05"
+    t.text "city"
+    t.text "province"
+    t.text "country"
+    t.bigint "affiliation_id"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "authors", force: :cascade do |t|
-    t.string "last_name"
-    t.string "given_name"
-    t.string "orcid"
+    t.text "last_name"
+    t.text "given_name"
+    t.text "orcid"
     t.boolean "isap"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "cr_affiliations", force: :cascade do |t|
-    t.string "name"
-    t.integer "article_author_id"
-    t.integer "author_affiliation_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "name"
+    t.text "article_author_id"
+    t.text "author_affiliation_id"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "cr_publications", force: :cascade do |t|
-    t.string "authors"
-    t.integer "pub_year"
-    t.string "title"
-    t.string "doi"
-    t.string "awards"
-    t.string "xref_affi"
-    t.string "themes"
-    t.integer "status"
-    t.string "note"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "cut_date"
+    t.text "authors"
+    t.bigint "pub_year"
+    t.text "title"
+    t.text "doi"
+    t.text "awards"
+    t.text "xref_affi"
+    t.text "themes"
+    t.bigint "status"
+    t.text "note"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
+    t.timestamptz "cut_date"
   end
 
   create_table "datasets", force: :cascade do |t|
-    t.string "complete"
-    t.string "description"
-    t.string "doi"
-    t.datetime "enddate"
-    t.string "location"
-    t.string "name"
-    t.datetime "startdate"
-    t.string "ds_type"
-    t.string "repository"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "complete"
+    t.text "description"
+    t.text "doi"
+    t.timestamptz "enddate"
+    t.text "location"
+    t.text "name"
+    t.timestamptz "startdate"
+    t.text "ds_type"
+    t.text "repository"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "organisation_aliases", force: :cascade do |t|
-    t.integer "organisation_id"
-    t.string "name"
-    t.string "alias_type"
-    t.datetime "valid_from"
-    t.datetime "valid_to"
-    t.string "laguage_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "organisation_id"
+    t.text "name"
+    t.text "alias_type"
+    t.timestamptz "valid_from"
+    t.timestamptz "valid_to"
+    t.text "laguage_code"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "organisations", force: :cascade do |t|
-    t.string "name"
-    t.string "short_name"
-    t.string "identifier"
-    t.string "logo"
-    t.string "homepage"
-    t.integer "address_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "country"
-    t.string "city"
-    t.string "sector"
-    t.string "region"
+    t.text "name"
+    t.text "short_name"
+    t.text "identifier"
+    t.text "logo"
+    t.text "homepage"
+    t.bigint "address_id"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
+    t.text "country"
+    t.text "city"
+    t.text "sector"
+    t.text "region"
   end
 
   create_table "sections", force: :cascade do |t|
-    t.string "obj_name"
-    t.string "name"
-    t.string "heading"
-    t.string "description"
+    t.text "obj_name"
+    t.text "name"
+    t.text "heading"
+    t.text "description"
     t.boolean "visible"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
   create_table "themes", force: :cascade do |t|
-    t.string "short"
-    t.string "name"
-    t.string "lead"
-    t.integer "phase"
-    t.string "used"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "theme_type"
+    t.text "short"
+    t.text "name"
+    t.text "lead"
+    t.bigint "phase"
+    t.text "used"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
+    t.text "theme_type"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.string "username"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.text "email", default: ""
+    t.text "encrypted_password", default: ""
+    t.text "reset_password_token"
+    t.timestamptz "reset_password_sent_at"
+    t.timestamptz "remember_created_at"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
+    t.text "confirmation_token"
+    t.timestamptz "confirmed_at"
+    t.timestamptz "confirmation_sent_at"
+    t.text "unconfirmed_email"
+    t.text "username"
+    t.index ["email"], name: "idx_24664_index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "idx_24664_index_users_on_reset_password_token", unique: true
   end
 
   create_table "xref_client_mappings", force: :cascade do |t|
-    t.string "obj_name"
-    t.string "origin"
-    t.string "target"
-    t.string "target_type"
-    t.string "default"
-    t.string "json_paths"
-    t.string "evaluate"
-    t.string "other"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.text "obj_name"
+    t.text "origin"
+    t.text "target"
+    t.text "target_type"
+    t.text "default"
+    t.text "json_paths"
+    t.text "evaluate"
+    t.text "other"
+    t.timestamptz "created_at"
+    t.timestamptz "updated_at"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id", name: "active_storage_attachments_blob_id_fkey"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id", name: "active_storage_variant_records_blob_id_fkey"
 
   create_view "inst_ctry_stats", sql_definition: <<-SQL
       SELECT v_ctry_inst.country,
@@ -307,19 +307,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_03_145548) do
                     GROUP BY article_authors.author_id, author_affiliations.short_name, author_affiliations.country) v_author_inst
             GROUP BY v_author_inst.country, v_author_inst.affi_name) v_ctry_inst
     GROUP BY v_ctry_inst.country;
-  SQL
-  create_view "list_themes", sql_definition: <<-SQL
-      SELECT themes.id,
-      themes.phase,
-      themes.name,
-      themes.short,
-      themes.lead,
-      count(*) AS article_count
-     FROM ((article_themes
-       JOIN themes ON ((article_themes.theme_id = themes.id)))
-       JOIN articles ON ((article_themes.article_id = articles.id)))
-    WHERE ((articles.status)::text = 'Added'::text)
-    GROUP BY themes.id, themes.phase, themes.name
-    ORDER BY themes.id;
   SQL
 end
