c8aeeac4179a5f63615a4e3d4d18cc1818257eae
Abraham <a_nieva@hotmail.com>
2026-08-27 19:56:33 +0100

    moved method to get collaborator data to model

--- app/models/author.rb
+++ app/models/author.rb
@@ -29,4 +29,62 @@ class Author < ApplicationRecord
     pr_name += self.last_name
     return pr_name
   end
+
+  def get_all_collaborators(filter)
+    # Get collaborations
+    collaborations = AuthorCollaboration.where(
+      "(author_source = ? OR author_target = ?) AND weight > 1",
+      self.id,
+      self.id
+    )
+
+    # Get initil id list for nodes:
+    node_ids = collaborations.flat_map do |edge|
+      [edge.author_source, edge.author_target]
+    end.uniq
+    # Get Nodes all nodes:
+    nodes = Author.where(id: node_ids).map do |author|
+      { data:{
+        id: author.id.to_s,
+        label: "#{author.get_abreviated_name}"},
+        classes: (author.id == self.id ? "central" : nil)
+      }
+    end
+    # get primary edges
+    edges = collaborations.map do |edge|
+      { data:
+        {
+          source: edge.author_source.to_s,
+          target: edge.author_target.to_s,
+          weight: edge.weight,
+          is_secondary: false
+        }
+      }
+    end
+    seen_pairs = Set.new
+    collaborations.each do |one_coll|
+      seen_pairs.add([one_coll.author_source, one_coll.author_target])
+    end
+    # get secondary edges
+    secondary_edges = AuthorCollaboration.where(
+      author_source: node_ids,
+      author_target: node_ids
+    )
+
+    secondary_edges.each do |an_edge|
+      if an_edge.weight > 1 # if one is through this coauthorship
+        pair = [an_edge.author_source, an_edge.author_target].sort
+        next if seen_pairs.include?(pair)
+        new_edge = {data: {
+            source: an_edge.author_source.to_s,
+            target: an_edge.author_target.to_s,
+            weight: an_edge.weight-1,
+            is_secondary: true
+            }
+          }
+        edges << new_edge
+      end
+    end
+    nodes + edges
+  end
 end
