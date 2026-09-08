8c9f7ff49ea5230a15d570d9f16228150e0adb13
Abraham <a_nieva@hotmail.com>
2026-09-08 15:24:56 +0100

    recreated cplas-temp

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -106,7 +106,7 @@ class AuthorsController < ApplicationController
     
     # get all collaborators for graph
     # parameter: min collaborations, filter inactive
-    @graph_data = @author.get_all_collaborators_fix(1, true)
+    @graph_data = @author.get_all_collaborators_fix(1, false)
     
     respond_to do |format|
       format.html
