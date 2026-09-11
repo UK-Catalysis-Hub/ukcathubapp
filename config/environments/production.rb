a91a912ff1ed173822f30c307e106908e6adcff2
Abraham <a_nieva@hotmail.com>
2026-09-11 10:22:16 +0100

    commented production.rb, see if that fixes deployment issues

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,10 +18,10 @@ Rails.application.configure do
   #config.action_controller.default_url_options = {
   #  host: ENV.fetch("APP_HOST", "localhost")
   #}
-  config.action_controller.default_url_options = {
-    host: ENV.fetch("APP_HOST", "localhost"),
-    protocol: ENV.fetch("APP_PROTOCOL", "http")
-  }
+  ##config.action_controller.default_url_options = {
+  ##  host: ENV.fetch("APP_HOST", "localhost"),
+  ##  protocol: ENV.fetch("APP_PROTOCOL", "http")
+  ##}
 
   # Ensures that a master key has been made available in ENV["RAILS_MASTER_KEY"], config/master.key, or an environment
   # key such as config/credentials/production.key. This key is used to decrypt credentials (and other encrypted files).
@@ -103,7 +103,7 @@ Rails.application.configure do
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 
-  config.hosts += ["localhost", "127.0.0.1", ENV.fetch("APP_HOST", "localhost")]
-  config.hosts.uniq!
+  ##config.hosts += ["localhost", "127.0.0.1", ENV.fetch("APP_HOST", "localhost")]
+  ##config.hosts.uniq!
 
 end
