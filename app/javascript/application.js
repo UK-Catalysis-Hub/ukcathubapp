5691dd7cd4ae4ec04e964348c0e0ba540526a350
Abraham <a_nieva@hotmail.com>
2026-09-16 12:06:19 +0100

    added turbo cacher

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,3 +4,13 @@ import "./controllers"
 import * as bootstrap from "bootstrap"
 import "chartkick/chart.js"
 
+document.addEventListener("turbo:before-fetch-response", (event) => {
+  const response = event.detail.fetchResponse.response;
+  
+  if (response.status === 429) {
+    event.preventDefault(); // Disconnects Turbo from replacing your view with a blank error state
+    
+    // Proactively show a localized toast warning or alert
+    alert("Calculations are processing! Please wait a moment before changing filters.");
+  }
+});
